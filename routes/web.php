be25aa5a9f3be8a2f7c43464da8c8d619a22e049
Fabian Espitia <fabian.espitia@repremundo.com.co>
2023-05-26 01:24:24 -0500

    Realizamos modificaciones en el README con descriocion de lo que hicimos

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [App\Http\Controllers\PortafolioController::class, 'index']);
 
-
 Auth::routes();
 
 Route::resource('/proyectos',App\Http\Controllers\ProyectoController::class);
